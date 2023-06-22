c03902ca990f08bdcee13c19f87bd8985ef4f95d
Stephen Okok <Stephok5@gmail.com>
2023-06-22 07:32:34 +0000

    PP

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,19 @@
-# fix 500 error 
-exec { 'wordpress typo error':
-  command  => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
-  path     => '/usr/bin:/usr/sbin:/bin',
+# 0-strace_is_your_friend.pp
+
+package { 'apache2':
+  ensure => installed,
 }
+
+service { 'apache2':
+  ensure => running,
+  enable => true,
+  require => Package['apache2'],
+}
+
+file { '/etc/apache2/apache2.conf':
+  ensure  => file,
+  content => '# Updated Apache configuration content',
+  require => Package['apache2'],
+  notify  => Service['apache2'],
+}
+
